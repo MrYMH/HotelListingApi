fc155d78e5297c7b79294d6713678ae33c79a9cb
MrYMH <https://github.com/MrYMH>
2023-04-19 10:43:01 +0200

    remove unused props

--- HotelListingApi.EF/Repositories/CountryRepository.cs
+++ HotelListingApi.EF/Repositories/CountryRepository.cs
@@ -12,13 +12,8 @@ namespace HotelListingApi.EF.Repositories
 {
     public class CountryRepository : BaseRepository<Country> , ICountryRepository
     {
-        private readonly ApplicationDbContext _context;
-        private readonly IMapper _mapper;
-
         public CountryRepository(ApplicationDbContext context , IMapper mapper ) : base( context  , mapper)
         {
-            _context= context;
-            _mapper= mapper;
         }
     }
 }
